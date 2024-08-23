0a4cdf88a54e438ac1d83fb6f880d924757506e3
Ton2xItsuraan <vantoineludel@gmail.com>
2024-08-24 07:40:50 +0800

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom"
+import { Header } from "./components"
+import MainContainer from "./components/MainContainer"
+import CreateContainer from "./components/CreateContainer"
+import { AnimatePresence } from "framer-motion"
+
+
+function App() {
+  
+
+  return (
+    <AnimatePresence>
+    <div className="w-screen h-auto flex flex-col bg-primary">
+      <Header />
+
+      <main className="mt-24 p-8 w-full">
+        <Routes>
+          <Route path="/*" element={<MainContainer/> } />
+          <Route path="/createItem" element={<CreateContainer/> } />
+        </Routes>
+      </main>
+    </div>
+    </AnimatePresence>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png"
+import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+  return (
+    <header className="fixed z-50 w-screen p-6 px-16">
+      {/* desktop & tablet */}
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+
+        <Link to={'/'} className="flex items-center gap-2">
+          <img src={Logo} className="w-20  object-cover" alt="Logo" />
+
+          <div>
+          <p className="text-headingColor text-xl font-bold">KALAKALAN</p>
+          <p className="text-headingColor text-xs font-bold ">Cafe & Resto</p>
+          </div>
+            
+          </Link>
+
+          <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8">
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              About Us
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Menu/Order
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Location
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Contact Us
+            </li>
+          </ul>
+
+          <div className="relative flex items-center justify-center">
+            <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
+          </div>
+
+          <motion.img whileTap={{scale: 0.6}} src={Avatar} alt="userprofile" className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" />
+          </div>
+      </div>
+
+      {/* mobile */}
+      <div className="flex md:hidden w-full h-full"></div>
+    </header>
+  );
+};
+
+export default Header;
